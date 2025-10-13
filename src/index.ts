13e7b6b6f4cdded1776308a71e77ffa7ddb85a2a
Abdulhafeez <laxhameedhafeez@gmail.com>
2025-10-13 20:03:23 +0200

    fix: remove express from root file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
-import express from "express";
 import * as dotenv from "dotenv";
 import { checkForNewContests, checkForNewRatingActivity } from "./services";
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3030;
-
-//middleware
-app.use(express.json());
-
 checkForNewContests();
 checkForNewRatingActivity();
 
-app.listen(port, () => console.log(`Listening on port ${port}!`));
